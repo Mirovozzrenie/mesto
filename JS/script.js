88792cfe6b0045e8799b14789a7eff6a58d3185d
Mirovozzrenie <noxpozitiv@gmail.com>
2020-10-19 15:17:44 +0300

    feature:cards from box

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,30 @@
-//Найдем в DOM основные кнопки 
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]
+;//Найдем в DOM основные кнопки 
 let profileEditButton = document.querySelector('.profile__edit-button');
 let popupCancelIcon = document.querySelector('.popup__cancel-icon');
 //найдем в DOM осн элементы 
@@ -8,7 +34,26 @@ let profileUserStatus = document.querySelector('.profile__user-status');
 let formElement = document.querySelector('.popup__edit-form');
 let nameInput = document.querySelector('.popup__user-name');
 let jobInput = document.querySelector('.popup__user-status');
+const elements = document.querySelector('.elements');
+//загатовка карточки для отрисовки 
+const cardTemplate = document.querySelector('.card__template');
 
+//функция добавления карточек 
+const renderElements = () => {
+const items =  initialCards.map(element => {
+    return `<div class="elements__card">
+        <img src="${initialCards.link}" alt="" class="elements__picture">
+        <div class="elements__place-panel">
+          <h2 class="elements__name">${initialCards.name}</h2>
+          <button class="elements__like" type="button"></button>
+        </div>
+      </div>`
+  }).join('');
+
+elements.insertAdjacentHTML('afterbegin', items);
+console.log(items);
+};
+renderElements();
 //функции видимости блока popup
 function popupActivation() {
   nameInput.value = profileUserName.textContent;
@@ -26,6 +71,7 @@ function formSubmitHandler(event) {
   profileUserStatus.textContent = jobInput.value;
   popupDeactivation();
 }
+   
 
 //добавим прослушку
 profileEditButton.addEventListener('click', popupActivation);
