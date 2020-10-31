d5510b3b7bae5d3d665fa25302685af89d81ba1a
Mirovozzrenie <noxpozitiv@gmail.com>
2020-10-31 13:02:58 +0300

    validation for econd form

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,37 +1,58 @@
 const newPlaceForm = document.querySelector('.popup__new-card-picture');
 const newPlaceName = newPlaceForm.querySelector('.popup__place-name');
 const newPlaceLink = newPlaceForm.querySelector('.popup__picture-link');
+const newProfileInfo = document.querySelector('.popup__new-profile-info')
 
-function showError(input){
-  const errorElement = newPlaceForm.querySelector(`#${input.id}-error`);
+function showError(formElements, input) {
+  const errorElement = formElements.querySelector(`#${input.id}-error`);
   errorElement.textContent = input.validationMessage;
-  input.classList.add('popup__input-field_state_invalid');
+  input.classList.add("popup__input-field_state_invalid");
 }
-function hideError(input){
-  const errorElement = newPlaceForm.querySelector(`#${input.id}-error`);
-  errorElement.textContent = ''; 
-  input.classList.remove('popup__input-field_state_invalid');
+function hideError(formElements, input) {
+  const errorElement = formElements.querySelector(`#${input.id}-error`);
+  errorElement.textContent = "";
+  input.classList.remove("popup__input-field_state_invalid");
 }
-function checkInputValidity(input){
+function checkInputValidity(formElements, input) {
   if (input.validity.valid) {
-    hideError(input);
-  }else {
-    showError(input);
+    hideError(formElements, input);
+  } else {
+    showError(formElements, input);
   }
 }
-function setEventListener() {
-const inputElements = Array.from(newPlaceForm.querySelectorAll('.popup__input-field'));
-  inputElements.forEach((input) => {
-  input.addEventListener('input', (event) => {
-    checkInputValidity(event.target);   
+function setEventListener(formElements, form) {
+  const inputElements = Array.from(
+    newPlaceForm.querySelectorAll(".popup__input-field")
+  );
+  const buttonElement = newPlaceForm.querySelector(".popup__save-cards");
+  inputElements.forEach(input => {
+    input.addEventListener("input", event => {
+      checkInputValidity(formElements, event.target);
+      toggleButtonState(formElements, buttonElement);
+    });
   });
-  });
-};
-function toggleButtonState(buttonElement){
-  
-};
+  toggleButtonState(formElements, buttonElement);
+}
 
-newPlaceForm.addEventListener('submit', function(event) {
+function toggleButtonState(formElements, buttonElement) {
+  if (formElements.checkValidity()) {
+    buttonElement.classList.remove("popup__save-button_invalid");
+    buttonElement.disabled = false;
+  } else {
+    buttonElement.classList.add("popup__save-button_invalid");
+    buttonElement.disabled = true;
+  }
+}
+
+function enableValidation() {
+    const formElements = Array.from(
+    newProfileInfo.querySelectorAll(".popup__input-field"));
+  formElements.forEach( form => {
+   form.addEventListener("submit", function(event) {
   event.preventDefault();
-}) 
-  setEventListener();
+});
+    setEventListener(form);
+  })
+}
+
+enableValidation();
